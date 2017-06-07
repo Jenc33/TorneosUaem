b2e169959add9cff990b0ca0c1b90970ed083172
Jorge Nieto <jorgeelihu33@gmail.com>
2017-06-07 11:39:22 -0500

    Creacion de all

---
+++ TorneosUAEM/src/java/jpa/entities/Students.java
@@ -0,0 +1,196 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author ingjo
+ */
+@Entity
+@Table(name = "STUDENTS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Students.findAll", query = "SELECT s FROM Students s")
+    , @NamedQuery(name = "Students.findById", query = "SELECT s FROM Students s WHERE s.id = :id")
+    , @NamedQuery(name = "Students.findByUsername", query = "SELECT s FROM Students s WHERE s.username = :username")
+    , @NamedQuery(name = "Students.findByPassword", query = "SELECT s FROM Students s WHERE s.password = :password")
+    , @NamedQuery(name = "Students.findByLevel", query = "SELECT s FROM Students s WHERE s.level = :level")
+    , @NamedQuery(name = "Students.findByName", query = "SELECT s FROM Students s WHERE s.name = :name")
+    , @NamedQuery(name = "Students.findByLastName", query = "SELECT s FROM Students s WHERE s.lastName = :lastName")
+    , @NamedQuery(name = "Students.findBySecondLastName", query = "SELECT s FROM Students s WHERE s.secondLastName = :secondLastName")
+    , @NamedQuery(name = "Students.findByAccountNumber", query = "SELECT s FROM Students s WHERE s.accountNumber = :accountNumber")})
+public class Students implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "USERNAME")
+    private String username;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "PASSWORD")
+    private String password;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "LEVEL")
+    private int level;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "NAME")
+    private String name;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    @Size(max = 50)
+    @Column(name = "SECOND_LAST_NAME")
+    private String secondLastName;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ACCOUNT_NUMBER")
+    private int accountNumber;
+    @JoinColumn(name = "ID_EQUIP", referencedColumnName = "ID")
+    @ManyToOne
+    private Equips idEquip;
+
+    public Students() {
+    }
+
+    public Students(Integer id) {
+        this.id = id;
+    }
+
+    public Students(Integer id, String username, String password, int level, String name, String lastName, int accountNumber) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.level = level;
+        this.name = name;
+        this.lastName = lastName;
+        this.accountNumber = accountNumber;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSecondLastName() {
+        return secondLastName;
+    }
+
+    public void setSecondLastName(String secondLastName) {
+        this.secondLastName = secondLastName;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Equips getIdEquip() {
+        return idEquip;
+    }
+
+    public void setIdEquip(Equips idEquip) {
+        this.idEquip = idEquip;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Students)) {
+            return false;
+        }
+        Students other = (Students) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "jpa.entities.Students[ id=" + id + " ]";
+    }
+    
+}
